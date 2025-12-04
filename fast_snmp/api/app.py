3de2af01a03ffdea037dc0f0d31d8b8c5bbfc5ef
xanv754 <angyeemarin.dev@gmail.com>
2025-12-04 09:10:35 -0400

    feat: add base to API

--- fast_snmp/api/app.py
+++ fast_snmp/api/app.py
@@ -1,9 +1,24 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home():
+    return {"detail": "Service is running..."}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
